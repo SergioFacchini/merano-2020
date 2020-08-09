460c4d65baacecbaf69cb3786b71e6294edece36
slava <slavarublev@gmail.com>
2020-08-09 13:02:30 +0200

    Updated publisher

--- publisher.js
+++ publisher.js
@@ -12,15 +12,17 @@ client.on('connect', function () {
     client.subscribe(settings.TOPIC, function (err) {
         console.log();
         setInterval(() => {
-            const data = {
-                mac: 'client-' + Math.floor(Math.random() * settings.MAX_CLIENTS),
-                bpm: Math.floor(Math.random() * 20 + 60),
-                timestamp: new Date().getTime()
-            };
-            console.log(data);
+            [0, 1, 2, 3, 4].forEach((i) => {
+                const data = {
+                    mac: 'client-' + i,
+                    bpm: Math.floor(Math.random() * 20 + 60),
+                    timestamp: new Date().getTime(),
+                    battery: Math.floor(Math.random() * 30 + 50),
+                };
+                client.publish(settings.TOPIC, JSON.stringify(data));
+            })
 
-            client.publish(settings.TOPIC, JSON.stringify(data));
-        }, 1000);
+        }, 7000);
     })
 })
 
